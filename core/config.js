809259d4b2eeccdfc3393f96de027de00c7fb873
Azamat9904 <azamatnamialy@gmail.com>
2020-12-06 15:11:18 +0600

    validation user handler

---
+++ core/config.js
@@ -0,0 +1,6 @@
+const faceRecognitionApi = {
+    apiKey: "3IijFw7U2nZJBbht2--tY_VUp-B-4DQr",
+    apiSecret: "9Plt3QzhOkCSkH3rs71YYeZw1VLK_XAA"
+}
+
+module.exports = faceRecognitionApi;
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const multer = require('multer');
 const upload = multer();
 const path = require('path');
 const UserModel = require('./UserModel.js');
-const { ObjectId } = require('mongodb');
+const imageToBase64 = require('image-to-base64');
+const request = require('request');
 
 require("./core/mongoose");
 
@@ -27,9 +28,13 @@ app.use(function (req, res, next) {
 
 const origin = 'http://localhost:3002';
 
+const getBase64Data = (image) => {
+    return image.replace(/^data:image\/jpeg;base64,/, "");
+}
+
 const createUserHandler = async (req, res, next) => {
     const name = req.body.name;
-    const base64Data = req.body.image.replace(/^data:image\/jpeg;base64,/, "");
+    const base64Data = getBase64Data(req.body.image);
     let imageName = name + Math.ceil(Math.random() * 100000) + '.jpeg';
     imageName = imageName.replace(" ", '');
     fs.writeFile(`uploads/${imageName}`, base64Data, 'base64', (err) => {
@@ -57,34 +62,6 @@ const getUsers = async (req, res) => {
     }
 }
 
-
-// const deleteUser = (req, res) => {
-//     const id = ObjectId(req.params.id);
-//     console.log(id);
-//     UserModel.findById(id, (err, user) => {
-//         if (err)
-// return res.status(404).json({ status: 'error', message: 'User is not found' });
-
-//         console.log(user, err);
-
-// let imagePath = user.imageUrl;
-// const paths = imagePath.split('/');
-// const imageName = paths[paths.length - 1];
-// imagePath = '/uploads/' + imageName;
-// console.log(imagePath);
-// fs.unlink(imagePath, (err) => {
-//     if (err)
-//         res.status(500).json({ status: 'error', message: 'Can not delete image' });
-
-//     user.remove().then(() => {
-//         res.json({ status: 'success', message: 'User successfullt deleted' });
-//     }).catch(() => {
-//         res.status(500).json({ status: 'error', message: 'Can not delete user' });
-//     });
-// });
-//     });
-// };
-
 const deleteUser = async (req, res, next) => {
     const id = req.params.id;
     try {
@@ -108,9 +85,60 @@ const deleteUser = async (req, res, next) => {
     }
 }
 
+const wait = (milleseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milleseconds))
+}
+
+const validateUser = async (req, res) => {
+    const image1 = req.body.image;
+    const prefix = "data:image/jpeg;base64,";
+    const users = await UserModel.find();
+
+    const results = [];
+
+    for (let user of users) {
+        const splits = user.imageUrl.split('/');
+        const imagePath = __dirname + '/uploads/' + splits[splits.length - 1];
+        let image2 = await imageToBase64(imagePath);
+        image2 = prefix + image2;
+
+        request.post({
+            url: 'https://api-us.faceplusplus.com/facepp/v3/compare', formData: {
+                api_key: 'wB0U2swa2c6Tabfv4ROQjScpe77DJ4iz',
+                api_secret: 'Q23bxMQPHNifNfDKYJdsGOoBtc9PGxTM',
+                image_base64_1: image1,
+                image_base64_2: image2
+            }
+        }, (err, httpResponse, body) => {
+            if (err) {
+                results.push({ status: 'error', error: err });
+            }
+            const result = JSON.parse(body);
+
+            if (result.error_message)
+                results.push({ status: 'error', result });
+            else
+                results.push({ status: 'success', result, user });
+        });
+        await wait(1500);
+    }
+
+    setTimeout(() => {
+        const positiveResults = results.filter((response) => {
+            if (response.status !== 'error' & response.result.confidence >= 60) {
+                return response;
+            }
+        });
+
+        res.json({ result: positiveResults });
+    }, 1000);
+}
+
 app.post('/user', createUserHandler);
 app.get('/user', getUsers);
 app.delete('/user/:id', deleteUser);
+app.post('/validateUser', validateUser);
+
 app.listen(port, () => {
     console.log('Server successfully started on port ' + port);
 });
